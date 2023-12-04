78b9ce00c3c09bf4908f86125b28e2ed1a2a46dc
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-05 00:37:05 +0530

    logged in

--- app.js
+++ app.js
@@ -5,6 +5,44 @@ function getKeyString(x, y) {
   return `${x}x${y}`;
 }
 (function (){
+
+   let playerId;
+   let playerRef;
+   let players = {};
+   let playerElements = {};
+   let coins = {};
+   let coinElements = {};
+
+  firebase.auth().onAuthStateChanged((user) => {
+    console.log(user);
+    if (user) {
+      //You're logged in!
+      playerId = user.uid;
+      playerRef = firebase.database().ref(`players/${playerId}`);
+
+      const name = createName();
+      playerNameInput.value = name;
+
+      const { x, y } = getRandomSafeSpot();
+
+      playerRef.set({
+        id: playerId,
+        name,
+        direction: "right",
+        color: randomFromArray(playerColors),
+        x,
+        y,
+        coins: 0,
+      });
+      
+    } else {
+      //You're logged out.
+    }
+  });
+
+
+
+
   firebase
     .auth()
     .signInAnonymously()
