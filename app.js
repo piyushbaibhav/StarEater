7702adc1c84a3a86f3151e246a7155ebfd6e7530
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-05 00:51:47 +0530

    remove dom

--- app.js
+++ app.js
@@ -85,6 +85,11 @@ function initGame() {
       characterElement.style.transform = `translate3d(${left}, ${top}, 0)`;
       gameContainer.appendChild(characterElement);
     })
+    allPlayersRef.on("child_removed", (snapshot) => {
+      const removedKey = snapshot.val().id;
+      gameContainer.removeChild(playerElements[removedKey]);
+      delete playerElements[removedKey];
+    });
 
 }
 
