abfaa4b674e2d289ce3957b9af564791dac6033a
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-05 00:33:22 +0530

    firebase

--- app.js
+++ app.js
@@ -4,3 +4,16 @@ function randomFromArray(array) {
 function getKeyString(x, y) {
   return `${x}x${y}`;
 }
+(function (){
+  firebase
+    .auth()
+    .signInAnonymously()
+    .catch((error) => {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // ...
+      console.log(errorCode, errorMessage);
+    });
+
+
+})();
