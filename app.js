66ffdddcea5c5d78611d803ac72cef4ef770114f
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-05 00:27:09 +0530

    .

--- app.js
+++ app.js
@@ -68,3 +68,43 @@ function createName() {
   ]);
   return `${prefix} ${animal}`;
 }
+
+function isSolid(x, y) {
+  const blockedNextSpace = mapData.blockedSpaces[getKeyString(x, y)];
+  return (
+    blockedNextSpace ||
+    x >= mapData.maxX ||
+    x < mapData.minX ||
+    y >= mapData.maxY ||
+    y < mapData.minY
+  );
+}
+
+function getRandomSafeSpot() {
+  
+  return randomFromArray([
+    { x: 1, y: 4 },
+    { x: 2, y: 4 },
+    { x: 1, y: 5 },
+    { x: 2, y: 6 },
+    { x: 2, y: 8 },
+    { x: 2, y: 9 },
+    { x: 4, y: 8 },
+    { x: 5, y: 5 },
+    { x: 5, y: 8 },
+    { x: 5, y: 10 },
+    { x: 5, y: 11 },
+    { x: 11, y: 7 },
+    { x: 12, y: 7 },
+    { x: 13, y: 7 },
+    { x: 13, y: 6 },
+    { x: 13, y: 8 },
+    { x: 7, y: 6 },
+    { x: 7, y: 7 },
+    { x: 7, y: 8 },
+    { x: 8, y: 8 },
+    { x: 10, y: 8 },
+    { x: 8, y: 8 },
+    { x: 11, y: 4 },
+  ]);
+}
