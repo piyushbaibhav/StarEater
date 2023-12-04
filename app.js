eed67f0435630ffecae996659cdae7e1af7e32ad
piyushbaibhav <piyushbaibhav12@gmail.com>
2023-12-05 01:12:18 +0530

    spot

--- app.js
+++ app.js
@@ -18,7 +18,34 @@ const mapData = {
     "9x9": true,
   },
 };
-
+function getRandomSafeSpot() {
+  //We don't look things up by key here, so just return an x/y
+  return randomFromArray([
+    { x: 1, y: 4 },
+    { x: 2, y: 4 },
+    { x: 1, y: 5 },
+    { x: 2, y: 6 },
+    { x: 2, y: 8 },
+    { x: 2, y: 9 },
+    { x: 4, y: 8 },
+    { x: 5, y: 5 },
+    { x: 5, y: 8 },
+    { x: 5, y: 10 },
+    { x: 5, y: 11 },
+    { x: 11, y: 7 },
+    { x: 12, y: 7 },
+    { x: 13, y: 7 },
+    { x: 13, y: 6 },
+    { x: 13, y: 8 },
+    { x: 7, y: 6 },
+    { x: 7, y: 7 },
+    { x: 7, y: 8 },
+    { x: 8, y: 8 },
+    { x: 10, y: 8 },
+    { x: 8, y: 8 },
+    { x: 11, y: 4 },
+  ]);
+}
 function randomFromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
 }
